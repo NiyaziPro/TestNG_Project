acf17978a839833714537efe4f6ecf971f480bf5
NiyaziPro <nyzjunior@gmail.com>
2024-09-10 16:06:36 +0200

    Auto-commit:

--- src/test/java/Test/C01_Test.java
+++ src/test/java/Test/C01_Test.java
@@ -1,4 +1,12 @@
 package Test;
 
+import org.openqa.selenium.json.JsonOutput;
+import org.testng.annotations.Test;
+
 public class C01_Test {
+
+    @Test
+    public void testName() {
+        System.out.println("Test");
+    }
 }
