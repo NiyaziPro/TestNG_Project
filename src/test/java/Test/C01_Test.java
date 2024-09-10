78cbede499792256cf42a43c4391fa2bdaf420ce
NiyaziPro <nyzjunior@gmail.com>
2024-09-10 16:17:13 +0200

    16:17 Tuesday 10.09.2024

--- src/test/java/Test/C01_Test.java
+++ src/test/java/Test/C01_Test.java
@@ -12,5 +12,6 @@ public class C01_Test {
         System.out.println("Test2");
         System.out.println("Test3");
         System.out.println("Test4");
+        System.out.println("Test5");
     }
 }
