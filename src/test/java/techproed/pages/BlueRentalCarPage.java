8ee170e55977343ac642d6b32b958296456952de
NiyaziPro <nyzjunior@gmail.com>
2024-09-11 10:05:27 +0200

    10:05 Wednesday 11.09.2024

---
+++ src/test/java/techproed/pages/BlueRentalCarPage.java
@@ -0,0 +1,30 @@
+package techproed.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import techproed.utilities.Driver;
+
+public class BlueRentalCarPage {
+
+    public BlueRentalCarPage() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
+    @FindBy(xpath = " //a[.=' Login']")
+    public WebElement loginButton;
+
+    @FindBy(id = "formBasicEmail")
+    public WebElement emailBox;
+
+    @FindBy(id = "formBasicPassword")
+    public WebElement passwordBox;
+
+    @FindBy(id = "dropdown-basic-button")
+    public WebElement dropDownMenuVerify;
+
+    @FindBy(xpath = "//*[contains(text(), 'not found')]")
+    public WebElement negativeLoginVerify;
+}
--- src/test/java/techproed/tests/day18_pageobjectmodel/C06_PropertiesUsage.java
+++ src/test/java/techproed/tests/day18_pageobjectmodel/C06_PropertiesUsage.java
@@ -1,14 +1,13 @@
 package techproed.tests.day18_pageobjectmodel;
 
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 import techproed.pages.AmazonPage;
 import techproed.pages.TechProEdPage;
 import techproed.utilities.ConfigReader;
 import techproed.utilities.Driver;
 
-public class C06_PropertiesTest {
+public class C06_PropertiesUsage {
 
 
     @Test
