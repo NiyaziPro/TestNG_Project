4c82c75a180f5f5c168241b74f7da6c236dbe3bf
NiyaziPro <nyzjunior@gmail.com>
2024-09-17 10:56:29 +0200

    10:56 Tuesday 17.09.2024

--- src/test/java/techproed/utilities/Listeners.java
+++ src/test/java/techproed/utilities/Listeners.java
@@ -1,10 +1,12 @@
 package techproed.utilities;
 
-import org.testng.ITestContext;
-import org.testng.ITestListener;
-import org.testng.ITestResult;
+import org.testng.*;
+import org.testng.annotations.ITestAnnotation;
 
-public class Listeners implements ITestListener {
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class Listeners implements ITestListener, IRetryAnalyzer, IAnnotationTransformer {
 
     @Override
     public void onStart(ITestContext context) {
@@ -33,6 +35,8 @@ public class Listeners implements ITestListener {
     @Override
     public void onTestFailure(ITestResult result) {
         System.out.println("onTestFailure  ==> sadece FAIL olan teslerden sonra bir kez  cagirilir! " + result.getName());
+        ReusableMethods.screenShot(result.getName());
+
 
     }
 
@@ -42,6 +46,37 @@ public class Listeners implements ITestListener {
 
     }
 
+    /*
+        Bu methodun amaci sadece bir test basarisiz oldugu zaman o testi yeniden kac defa calistiracağımızi belirlemektir
+        maxRetryCount = 1; ==>ek olarak calisma sayisidir
+        Bu ornekte fail olan test normal bir kere calistiktan sonra, bir kere daha bu methodun calismasini saglayacak.
+        buraya kac yazarsak o kadar tekrar calisir.
+         */
+    private static int retryCount = 0;
+    private static final int maxRetryCount = 1;
+
+    @Override
+    public boolean retry(ITestResult result) {
+        if (retryCount < maxRetryCount) {
+            retryCount++;
+            return true; //true döndürmesi, fail olan testin yeniden çalıştırılmasını sağlar
+        }
+        return false;
+    }
+
+/*
+Bu method bir test methoduna bir RetryAnalyzer eklemek icin kullanilir
+Listeners classindaki retry methodunu yani yukarida yazmis oldugumuz methodu
+RetryAnalyzer olarak ayarlar
+Bu sayede test fail oldugu zaman Listeners clasimizdaki
+bu retry methodu cagrilir ve yukaridaki tekar calistirma uygulanmis olacak
+ */
+
+    @Override
+    public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
+
+        annotation.setRetryAnalyzer(Listeners.class);
+    }
 
 
 }
