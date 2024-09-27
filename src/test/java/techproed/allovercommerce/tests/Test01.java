baf082e1cba41b35d6a9607a79bf5ffb8e2916b4
NiyaziPro <nyzjunior@gmail.com>
2024-09-27 18:30:46 +0200

    18:30 Friday 27.09.2024

--- src/test/java/techproed/allovercommerce/tests/Test01.java
+++ src/test/java/techproed/allovercommerce/tests/Test01.java
@@ -1,19 +1,39 @@
 package techproed.allovercommerce.tests;
 
+import com.github.javafaker.Faker;
 import org.testng.annotations.Test;
 import techproed.allovercommerce.pages.MainPages;
 import techproed.utilities.Driver;
 import techproed.utilities.WaitUtils;
+
+import java.util.Locale;
 
 public class Test01 {
 
     @Test
     public void test01() {
         MainPages mainPages = new MainPages();
+        Faker faker = new Faker(Locale.of("tr", "tr"));
 
         Driver.getDriver().get("https://www.allovercommerce.com/");
+        mainPages.homePage.register.click();
+        WaitUtils.waitFor(3);
 
-        mainPages.homePage.signIn.click();
-        mainPages.loginPage.lostYourPasswordLink.click();
+        String username = faker.name().username();
+        System.out.println("Username : " + username);
+        mainPages.registerPage.usernameBox.sendKeys(username);
+
+        String email = faker.internet().emailAddress(username);
+        System.out.println("Email : "+email);
+        mainPages.registerPage.emailBox.sendKeys(email);
+
+        String password = faker.internet().password(8,12,true,true,true);
+        System.out.println("Password : " + password);
+        mainPages.registerPage.passwordBox.sendKeys(password);
+
+        mainPages.registerPage.iAgreetoCheckBox.click();
+
+
+
     }
 }
